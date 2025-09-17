d5236d11e620451cce89548bdc26cdb6065a82d5
Kyle Huang <kylehuang@todd.imp.fu-berlin.de>
2025-09-17 15:23:04 +0200

    initialized test cases file

---
+++ runtest.jl
@@ -0,0 +1,14 @@
+#Test cases for SATUNITRI functions
+
+using Combinatorics
+using LinearAlgebra
+using Polyhedra
+using PicoSAT
+using Dates
+using Printf
+using Base.Threads
+using TOML
+using Random
+
+include("main.jl")
+
