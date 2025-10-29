6bae1e4816f46ac75e2675135c98805feba7c348
rlauff <r.lauff1997@gmail.com>
2025-10-29 15:05:37 +0100

    fixed test.jl

--- test.jl
+++ test.jl
@@ -2,7 +2,10 @@
 include("Triangulate.jl")
 using .Triangulate
 
+print("-"*70)
 print("Test 1, Vol 6 3D. Expect 43 triangulatable polytopes")
+print("-"*70)
+print("\n")
 triangulate(
     "Polytopes/small-lattice-polytopes/data/3-polytopes/v6.txt",
     intersection_backend="cpu",
@@ -13,8 +16,10 @@ triangulate(
     validate=false
     )
 
-
+print("-"*70)
 print("Test 2, Vol 12 3D. Expect 745 triangulatable polytopes")
+print("-"*70)
+print("\n")
 triangulate(
     "Polytopes/small-lattice-polytopes/data/3-polytopes/v12.txt",
     intersection_backend="cpu",
@@ -27,6 +32,8 @@ triangulate(
 
 
 print("Test 3, Vol 16 3D. Expect 3288 triangulatable polytopes")
+print("-"*70)
+print("\n")
 triangulate(
     "Polytopes/small-lattice-polytopes/data/3-polytopes/v16.txt",
     intersection_backend="cpu",
