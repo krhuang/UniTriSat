18a1b52900c7b9fb3fc5083dc0bcdd1a16204b90
rlauff <r.lauff1997@gmail.com>
2025-11-01 12:50:28 +0100

    added timings to test.jl

--- test.jl
+++ test.jl
@@ -1,31 +1,24 @@
 using StyledStrings
-
+using Printf
 
 include("Triangulate.jl")
 using .Triangulate
 
-
-test_results = []
-
-println("-")
-println(styled"{bold, blue:Test 1, Bench 3D. Expect 100% triangulatable polytopes}")
-println("-")
-results  = triangulate(
-    "Polytopes/bench",
-    terminal_output="running, table",
-    )
-num_triangulatable = length([1 for result in results if result.num_solutions_found>0])
-expected_number = length(results)
-if  num_triangulatable == expected_number
-    println(styled"{bold, green:passed}\n")
-    push!(test_results, "passed")
-else
-    println(styled"{bold, red:failed}, Expected $expected_number, got $num_triangulatable")
-    push!(test_results, "failed: Expected $expected_number, got $num_triangulatable")
+function format_duration(total_seconds::Float64)
+    total_seconds_int = floor(Int, total_seconds)
+    h = total_seconds_int ÷ 3600
+    rem_seconds = total_seconds_int % 3600
+    m = rem_seconds ÷ 60
+    s = rem_seconds % 60
+    return @sprintf("%02d:%02d:%02d", h, m, s)
 end
 
+test_results = []
+test_num = 1
+
+
 println("-")
-println(styled"{bold, blue:Test 2, Vol 6 3D. Expect 43 triangulatable polytopes}")
+println(styled"{bold, blue:Test $test_num, Vol 6 3D. Expect 43 triangulatable polytopes}")
 println("-")
 results  = triangulate(
     "Polytopes/small-lattice-polytopes/data/3-polytopes/v6.txt",
@@ -33,16 +26,18 @@ results  = triangulate(
     )
 num_triangulatable = length([1 for result in results if result.num_solutions_found>0])
 expected_number = 43
+total_time = sum([result.total_time for result in results])
 if  num_triangulatable == expected_number
-    println(styled"{bold, green:passed}\n")
-    push!(test_results, "passed")
+    println(styled"{bold, green:passed} in $(format_duration(total_time))\n")
+    push!(test_results, "passed in $(format_duration(total_time))")
 else
     println(styled"{bold, red:failed}, Expected $expected_number, got $num_triangulatable")
     push!(test_results, "failed: Expected $expected_number, got $num_triangulatable")
 end
+test_num += 1
 
 println("-")
-println(styled"{bold, blue:Test 3, Vol 12 3D. Expect 745 triangulatable polytopes}")
+println(styled"{bold, blue:Test $test_num, Vol 12 3D. Expect 745 triangulatable polytopes}")
 println("-")
 results = triangulate(
     "Polytopes/small-lattice-polytopes/data/3-polytopes/v12.txt",
@@ -50,16 +45,18 @@ results = triangulate(
     )
 num_triangulatable = length([1 for result in results if result.num_solutions_found>0])
 expected_number = 745
-if  num_triangulatable == expected_number
-    println(styled"{bold, green:passed}\n")
-    push!(test_results, "passed")
+total_time = sum([result.total_time for result in results])
+    if  num_triangulatable == expected_number
+        println(styled"{bold, green:passed} in $(format_duration(total_time))\n")
+    push!(test_results, "passed in $(format_duration(total_time))")
 else
     println(styled"{bold, red:failed}, Expected $expected_number, got $num_triangulatable")
     push!(test_results, "failed: Expected $expected_number, got $num_triangulatable")
 end
+test_num += 1
 
 println("-")
-println(styled"{bold, blue:Test 4, Vol 16 3D. Expect 3288 triangulatable polytopes}")
+println(styled"{bold, blue:Test $test_num, Vol 16 3D. Expect 3288 triangulatable polytopes}")
 println("-")
 results = triangulate(
     "Polytopes/small-lattice-polytopes/data/3-polytopes/v16.txt",
@@ -67,16 +64,18 @@ results = triangulate(
     )
 num_triangulatable = length([1 for result in results if result.num_solutions_found>0])
 expected_number = 3288
-if  num_triangulatable == expected_number
-    println(styled"{bold, green:passed}\n")
-    push!(test_results, "passed")
+total_time = sum([result.total_time for result in results])
+    if  num_triangulatable == expected_number
+        println(styled"{bold, green:passed} in $(format_duration(total_time))\n")
+    push!(test_results, "passed in $(format_duration(total_time))")
 else
     println(styled"{bold, red:failed}, Expected $expected_number, got $num_triangulatable")
     push!(test_results, "failed: Expected $expected_number, got $num_triangulatable")
 end
+test_num += 1
 
 println("-")
-println(styled"{bold, blue:Test 5, Vol 10 4D. Expect 618 triangulatable polytopes}")
+println(styled"{bold, blue:Test $test_num, Vol 10 4D. Expect 618 triangulatable polytopes}")
 println("-")
 results = triangulate(
     "Polytopes/small-lattice-polytopes/data/4-polytopes/v10.txt",
@@ -84,16 +83,18 @@ results = triangulate(
     )
 num_triangulatable = length([1 for result in results if result.num_solutions_found>0])
 expected_number = 618
+total_time = sum([result.total_time for result in results])
 if  num_triangulatable == expected_number
-    println(styled"{bold, green:passed}\n")
-    push!(test_results, "passed")
+    println(styled"{bold, green:passed} in $(format_duration(total_time))\n")
+    push!(test_results, "passed in $(format_duration(total_time))")
 else
     println(styled"{bold, red:failed}, Expected $expected_number, got $num_triangulatable")
     push!(test_results, "failed: Expected $expected_number, got $num_triangulatable")
 end
+test_num += 1
 
 println("-")
-println(styled"{bold, blue:Test 6, Vol 10 5D. Expect 841 triangulatable polytopes}")
+println(styled"{bold, blue:Test $test_num, Vol 10 5D. Expect 841 triangulatable polytopes}")
 println("-")
 results = triangulate(
     "Polytopes/small-lattice-polytopes/data/5-polytopes/v10.txt",
@@ -101,16 +102,18 @@ results = triangulate(
     )
 num_triangulatable = length([1 for result in results if result.num_solutions_found>0])
 expected_number = 841
+total_time = sum([result.total_time for result in results])
 if  num_triangulatable == expected_number
-    println(styled"{bold, green:passed}\n")
-    push!(test_results, "passed")
+    println(styled"{bold, green:passed} in $(format_duration(total_time))\n")
+    push!(test_results, "passed in $(format_duration(total_time))")
 else
     println(styled"{bold, red:failed}, Expected $expected_number, got $num_triangulatable")
     push!(test_results, "failed: Expected $expected_number, got $num_triangulatable")
 end
+test_num += 1
 
 println("-")
-println(styled"{bold, blue:Test 7, Vol 10 6D. Expect 959 triangulatable polytopes}")
+println(styled"{bold, blue:Test $test_num, Vol 10 6D. Expect 959 triangulatable polytopes}")
 println("-")
 results = triangulate(
     "Polytopes/small-lattice-polytopes/data/6-polytopes/v10.txt",
@@ -118,17 +121,19 @@ results = triangulate(
     )
 num_triangulatable = length([1 for result in results if result.num_solutions_found>0])
 expected_number = 959
+total_time = sum([result.total_time for result in results])
 if  num_triangulatable == expected_number
-    println(styled"{bold, green:passed}\n")
-    push!(test_results, "passed")
+    println(styled"{bold, green:passed} in $(format_duration(total_time))\n")
+    push!(test_results, "passed in $(format_duration(total_time))")
 else
     println(styled"{bold, red:failed}, Expected $expected_number, got $num_triangulatable")
     push!(test_results, "failed: Expected $expected_number, got $num_triangulatable")
 end
+test_num += 1
 
 
 for (i,res) in enumerate(test_results)
-    if res == "passed"
+    if startswith(res, "passed")
         println(styled"Test $i: {bold, green: passed}")
     else
         println(styled"Test $i: {bold, red:$res}")
