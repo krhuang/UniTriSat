ce998a8574b3a8437132722ad1bae855e70de2ad
Charles Zhang <charleszhang99@yahoo.com>
2025-11-05 16:56:19 +0100

    Switch from BigInt to Int and use det_bareiss.
    The determinant computation on Int matrices returns floats, so call
    out to det_bareiss instead when computing all unimodular simplices.
    
    Timings on my laptop:
    
    With BigInt det: ~73 iters per ms.
    With imprecise Int det + rounding: ~2100-2200 iters per ms.
    With det_bareiss (fraction free GE): ~3300-3500 iters per ms.
    
    where BigInt det was the old code.

--- basic_computations.jl
+++ basic_computations.jl
@@ -34,7 +34,7 @@ function lattice_points_via_Normaliz(vertices::Matrix{Int})
         end
     end
 
-    return [BigInt(vec[j]) for vec in points, j in 1:ncols]
+    return [vec[j] for vec in points, j in 1:ncols]
 end
 
 function lattice_points_via_Oscar(vertices::Matrix{Int})
@@ -43,7 +43,7 @@ function lattice_points_via_Oscar(vertices::Matrix{Int})
     dims = size(LP)
     nrows = dims[1]
     ncols = size(LP[1])[1]
-    julia_matrix_LP = [BigInt(LP[i][j]) for i in 1:nrows, j in 1:ncols]
+    julia_matrix_LP = [LP[i][j] for i in 1:nrows, j in 1:ncols]
     return julia_matrix_LP
 end
 
@@ -61,7 +61,7 @@ function next_combination!(inds::Vector{Int}, n::Int)
     return false  # no next combination
 end
 
-function all_simplices(lattice_points::Matrix{BigInt}; only_unimodular::Bool=false)
+function all_simplices(lattice_points::Matrix{Int}; only_unimodular::Bool=false)
     n, d = size(lattice_points)
     simplex_indices = NTuple{d+1,Int}[]
     if n < d + 1
@@ -69,7 +69,7 @@ function all_simplices(lattice_points::Matrix{BigInt}; only_unimodular::Bool=fal
     end
 
     inds = collect(1:(d+1))             # initial combination
-    diffs = Matrix{BigInt}(undef, d, d)
+    diffs = Matrix{Int}(undef, d, d)
 
     while true
         idx_1 = inds[1]
@@ -79,7 +79,7 @@ function all_simplices(lattice_points::Matrix{BigInt}; only_unimodular::Bool=fal
                 diffs[j, i] = lattice_points[idx_j, i] - lattice_points[idx_1, i]
             end
         end
-        det_val = det(diffs)
+        det_val = LinearAlgebra.det_bareiss(diffs) # use exact integer determinant
         if det_val != 0 && (!only_unimodular || abs(det_val) == 1)
             push!(simplex_indices, Tuple(inds))
         end
@@ -91,12 +91,12 @@ end
 function rational_plane_to_integer(plane)
     denoms = [denominator(x) for x in plane.a]
     lcm_d = foldl(lcm, denoms)
-    int_a = map(x -> BigInt(x * lcm_d), plane.a)
-    int_β = BigInt(plane.β * lcm_d)
+    int_a = map(x -> Int(x * lcm_d), plane.a)
+    int_β = Int(plane.β * lcm_d)
     return int_a, int_β
 end
 
-function internal_faces(vertices::Matrix{BigInt}, dim::Int)
+function internal_faces(vertices::Matrix{Int}, dim::Int)
     n = size(vertices, 1)
     if n < dim
         return Set{NTuple{dim, Int}}()
@@ -110,8 +110,8 @@ function internal_faces(vertices::Matrix{BigInt}, dim::Int)
     inds = collect(1:dim)
     faces = Set{NTuple{dim, Int}}()
 
-    planes_a = Vector{Vector{BigInt}}(undef, length(rational_planes))
-    planes_β = Vector{BigInt}(undef, length(rational_planes))
+    planes_a = Vector{Vector{Int}}(undef, length(rational_planes))
+    planes_β = Vector{Int}(undef, length(rational_planes))
     for i in 1:length(rational_planes)
         planes_a[i], planes_β[i] = rational_plane_to_integer(rational_planes[i])
     end
