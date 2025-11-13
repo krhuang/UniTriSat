a31804ff2af67a79b93580c2aede2d778059e416
Kyle Huang <kylehuang@cofio.imp.fu-berlin.de>
2025-11-13 13:54:30 +0100

    fixing subdivision regularity triangulation input type

--- src/subdivision_regularity.jl
+++ src/subdivision_regularity.jl
@@ -2,121 +2,78 @@ module SubdivisionRegularity
 	export is_regular
 	using Polyhedra, CDDLib, Combinatorics, LinearAlgebra
 
-	function internal_faces(triangulation::Vector{Vector{Int}}, dimension::Int)
-	    counts = Dict{Set{Int}, Int}()
+	# Helper to treat the rows of a matrix as coordinate tuples
+	pointset(mat::Matrix{Int}) = Set(Tuple.(eachrow(mat)))
+
+	# Compute all internal faces shared by exactly two simplices
+	function internal_faces(triangulation::Vector{Matrix{Int}}, dimension::Int)
+	    counts = Dict{Set{NTuple{N,Int}} where N, Int}()
 	    for simplex in triangulation
-			for face in combinations(simplex, dimension)
-				s = Set(face)
+			for face_rows in combinations(1:size(simplex,1), dimension)
+				face = simplex[collect(face_rows), :]
+				s = pointset(face)
 				counts[s] = get(counts, s, 0) + 1
 			end
 	    end
-	    return [f for (f, c) in counts if c == 2]
+	    # Faces that occur exactly twice are internal
+	    return [reduce(vcat, [collect(x)' for x in f]) for (f, c) in counts if c == 2]
 	end
 
-	function adjacent_simplices(triangulation::Vector{Vector{Int}}, face::Set{Int})
-	    [s for s in triangulation if issubset(face, s)]
+	# Find the two simplices adjacent along a given face
+	function adjacent_simplices(triangulation::Vector{Matrix{Int}}, face::Matrix{Int})
+	    fset = pointset(face)
+	    [s for s in triangulation if issubset(fset, pointset(s))]
 	end
 
 	# This is a mess. TODO: Fix it when you get a chance
-	function is_regular(points::Matrix{Rational{BigInt}}, triangulation::Vector{Vector{Int}})
-	    n, dimension = size(points)
+	function is_regular(triangulation::Vector{Matrix{Int}})
+	    dimension = size(first(triangulation), 2)
+	    points = unique(vcat(triangulation...; dims=1))
+	    n = size(points, 1)
 	    internal = internal_faces(triangulation, dimension)
-	    println(size(internal))
-	    A = Vector{Vector{Rational{BigInt}}}()  # list of rows
+	    println("Number of internal faces: ", length(internal))
+
+	    A = Vector{Vector{Int}}()  # list of rows
 
 	    for face in internal
 			adj = adjacent_simplices(triangulation, face) # TODO: this should just compute a circuit with the first and last points being special
 			if length(adj) == 2
 				s1, s2 = adj
-				p = only(setdiff(s1, face))
-				q = only(setdiff(s2, face))
-				circuit = vcat(s1, q)
+				p = only(setdiff(pointset(s1), pointset(face)))
+				q = only(setdiff(pointset(s2), pointset(face)))
+				p_vec = collect(p)'
+				q_vec = collect(q)'
+				circuit = vcat(s1, q_vec)
 
 				# Constructing the folding form
 				# See Definition 5.2.4 "Folding Form" of De Loera, Rambau, Santos - Triangulations: Structures for Algorithms and Applications
 
-				submatrix = points[s1, :]
-				# sign/orientation computation for the simplex
-				aug = hcat(submatrix, ones(Rational{BigInt}, size(submatrix,1)))  # hcat adds a column
-					sign_det = LinearAlgebra.det_bareiss(aug)
+				submatrix = hcat(s1, ones(Int, size(s1, 1)))  # hcat adds a column
+				sign_det = round(Int, det(submatrix))
 				# @assert sign_det = 1 || sign_det = -1 # Input is assumed to be unimodular
 
-
-				folding_form = zeros(Rational{BigInt}, n)
+				folding_form = zeros(Int, n)
 				# Constructing one folding form
-				for (i, point_index) in enumerate(circuit)
-					submatrix = points[setdiff(circuit, point_index), :]            # select rows
-						augmented = hcat(submatrix, ones(Rational{BigInt}, size(submatrix, 1)))  # add column of ones
-						folding_form[point_index] = (-1)^i * LinearAlgebra.det_bareiss(augmented)
+				for (i, row_idx) in enumerate(1:size(circuit, 1))
+					submatrix = circuit[setdiff(1:size(circuit, 1), [row_idx]), :]  # select rows
+					augmented = hcat(submatrix, ones(Int, size(submatrix, 1)))  # add column of ones
+					val = (-1)^i * round(Int, det(augmented))
+					pt = circuit[row_idx, :]
+					idx = findfirst(r -> all(r .== pt), eachrow(points))
+					folding_form[idx] = val
 				end
-				push!(A, sign_det*folding_form) #sign_det should actually be sign
+				push!(A, sign_det * folding_form) # sign_det should actually be sign
 			end
 	    end
 
 	    # Convert list of rows to matrix
 	    folding_forms_matrix = reduce(vcat, permutedims.(A))
-	    b = zeros(Rational{BigInt}, size(folding_forms_matrix, 1))
+	    b = zeros(Int, size(folding_forms_matrix, 1))
 
 	    # Build exact polyhedron
-	   	
-		P = polyhedron(hrep(folding_forms_matrix, b), CDDLib.Library(:exact))
+	   	P = polyhedron(hrep(folding_forms_matrix, b), CDDLib.Library(:exact))
 
 	    # Check full dimensionality (open feasibility)
 	    return dim(P) == n
 	end
-
-	#=
-	points2D = Rational{BigInt}.([
-	    0 0;
-	    1 0;
-	    0 1;
-	    1 1
-	])
-	triangulation2D = [
-	    [1,2,3],
-	    [2,4,3]
-	]
-	println("Regular? ", is_regular(points2D, triangulation2D))
-
-	# Mother of all examples
-
-	points_nonreg = Rational{BigInt}.([
-		0 0; #1
-		1 0; #2
-		2 0; #3
-		3 0; #4
-		4 0; #5
-		0 1; #6
-		1 1; #7
-		2 1; #8
-		3 1; #9
-		0 2; #10
-		1 2; #11
-		2 2; #12
-		0 3; #13
-		1 3; #14
-		0 4 #15
-	])
-
-	triangulation_nonreg = [
-		[1, 6, 7],
-		[1, 7, 8],
-		[1, 2, 8],
-		[2, 3, 8],
-		[3, 4, 8],
-		[4, 5, 8],
-		[5, 8, 11],
-		[5, 9, 11],
-		[6, 7, 10],
-		[7, 10, 13],
-		[7, 13, 15],
-		[7, 11, 15],
-		[7, 8, 11],
-		[9, 11, 12],
-		[11, 12, 14],
-		[11, 14, 15]
-	]
-
-	println("The mother of all examples is regular?", is_regular(points_nonreg, triangulation_nonreg))
-	=#
 end
