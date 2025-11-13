d25fc19584ef3c5b700985db78fe67f3b5534a66
Kyle Huang <kylehuang@cofio.imp.fu-berlin.de>
2025-11-13 13:59:40 +0100

    modified test example of is_regular input-type change

--- test.jl
+++ test.jl
@@ -163,45 +163,29 @@ println("-")
 println(styled"{bold, blue:Test $test_num, $test_name. Expect non-regular}")
 println("-")
 
-points_nonreg = Rational{BigInt}.([
-    0 0; #1
-    1 0; #2
-    2 0; #3
-    3 0; #4
-    4 0; #5
-    0 1; #6
-    1 1; #7
-    2 1; #8
-    3 1; #9
-    0 2; #10
-    1 2; #11
-    2 2; #12
-    0 3; #13
-    1 3; #14
-    0 4  #15
-])
 
+# Define triangulation directly as a vector of matrices of integer coordinates
 triangulation_nonreg = [
-    [1, 6, 7],
-    [1, 7, 8],
-    [1, 2, 8],
-    [2, 3, 8],
-    [3, 4, 8],
-    [4, 5, 8],
-    [5, 8, 11],
-    [5, 9, 11],
-    [6, 7, 10],
-    [7, 10, 13],
-    [7, 13, 15],
-    [7, 11, 15],
-    [7, 8, 11],
-    [9, 11, 12],
-    [11, 12, 14],
-    [11, 14, 15]
+    [0 0; 0 1; 1 1],
+    [0 0; 1 1; 2 1],
+    [0 0; 1 0; 2 1],
+    [1 0; 2 0; 2 1],
+    [2 0; 3 0; 2 1],
+    [3 0; 4 0; 2 1],
+    [4 0; 2 1; 1 2],
+    [4 0; 3 1; 1 2],
+    [0 1; 1 1; 0 2],
+    [1 1; 0 2; 1 3],
+    [1 1; 1 3; 0 4],
+    [1 1; 1 2; 0 4],
+    [1 1; 2 1; 1 2],
+    [3 1; 1 2; 2 2],
+    [1 2; 2 2; 1 3],
+    [1 2; 1 3; 0 4]
 ]
 
 elapsed_time = @elapsed begin
-    regularity_result = is_regular(points_nonreg, triangulation_nonreg)
+    regularity_result = is_regular(triangulation_nonreg)
 end
 
 if regularity_result
