a1d41b972073796e658559997884d45d5468d41f
Kyle Huang <krhuang5@gmail.com>
2026-02-24 15:13:35 +0100

    fano matroid base polytope example and script

---
+++ fano_matroid.jl
@@ -0,0 +1,25 @@
+import Pkg
+Pkg.activate(".")
+
+using StyledStrings
+using Printf
+using UniTriSat
+using Polyhedra
+
+include("src/helpers.jl")
+using .Helpers
+
+
+
+terminal_output = "initial, running, table, final" #initial, running, table, final
+
+println("-")
+println(styled"{bold, blue:Fano Matroid}")
+println("-")
+triangulate(
+    "Polytopes/fano_matroid_base_polytope",
+    terminal_output=terminal_output,
+    regular=true,
+    solver="cadical"
+    )
+
